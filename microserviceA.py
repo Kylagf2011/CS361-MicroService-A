83e74cccc639056c32f8096eb1ee0635e536563b
Kylagf2011 <41589662+Kylagf2011@users.noreply.github.com>
2025-02-23 22:31:54 -0800

    Add files via upload

---
+++ microserviceA.py
@@ -0,0 +1,43 @@
+import zmq # type: ignore
+import sys
+import time
+
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:5555")
+
+definitions = {
+    "Bar Chart": "Graph which compares data using bars to represent values. They are often used for categorical data.",
+    "Line Graph": "Graph which uses lines to connect data points. They are often used to show how a variable changes over time.",
+    "Scatter Plot": "Graph which uses dots to represent values. They are often used for larger, more spread datasets."
+}
+    
+# Convert the input term to lowercase and check for matching term
+while True:
+    try:
+        term = socket.recv_string()
+
+        if len(term) > 0:
+            print("Message Recieved: %s\n" % term)
+
+        if term == "exit":
+            break
+
+        term_lower = term.lower()
+        found = False
+
+        for key in definitions:
+            if key.lower() == term_lower:
+                socket.send_string(definitions[key])
+                found = True
+                break
+
+        if not found:
+            socket.send_string("Sorry, that definition could not be found.")
+    
+    except zmq.ZMQError as e:
+        print(f"Error encountered while trying to send/receive: {e}")
+        break
+
+print("Goodbye!\n")
+context.destroy()
